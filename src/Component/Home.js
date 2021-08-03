414101c967aad273a85c22a07014a65b7ae45e23
Zebaanikanibir <zanikanibir@gmail.com>
2021-08-03 17:48:22 +0600

    done4

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
     return (
         <div className="home">
             <div className="line">
-                <h4 style={{ color: '#3A72B7' }}>Mcq Of The Day</h4>
-                <h6 style={{ color: '#3A72B7' }}>Topic Summery</h6>
+                <h4 style={{ color: '#3A72B7',marginRight:'8px',marginTop:'20px'}}>MCQ Of The Day</h4>
+                <h6 style={{ color: '#3A72B7',marginTop:'23px' }}>Topic Summery</h6>
                 
-                <svg style={{ width: '22px',color: '#3A72B7' }} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg style={{ width: '22px',color: '#3A72B7',marginTop:'25px',marginLeft:'25px'}} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
             </div>
