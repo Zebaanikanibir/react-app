ef55cce9b07a4b29571dd53ccff8c475aeb01c0a
Zebaanikanibir <zanikanibir@gmail.com>
2021-08-03 16:55:55 +0600

    done2

---
+++ src/Component/Box.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className="box0">
+     
+            <div className="d-flex box2">
+                <div className="line1">
+                    <p>A</p>
+                </div>
+                <p className="para">2000 years Old</p>
+            </div>
+            <div className="d-flex box2">
+                <div className="line1">
+                    <p>B</p>
+                </div>
+                <p className="para">45 BC, Make it Over </p>
+            </div>
+            <div className="d-flex box2">
+                <div className="line1">
+                    <p>C</p>
+                </div>
+                <p className="para">Lorem gimpk kollp ral</p>
+            </div>
+            <div className="d-flex box2">
+                <div className="line1">
+                    <p>D</p>
+                </div>
+                <p className="para">Kap hara rama hweo</p>
+            </div>
+            
+    </div>
+  )
+}
+
+export default Box
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Box from './Box';
 
 import './Home.css';
 const Home = () => {
@@ -20,30 +21,7 @@ const Home = () => {
                 </p>
             </div>
 
-            <div className="d-flex box2">
-                <div className="line1">
-                    <p>A</p>
-                </div>
-                <p className="para">2000 years Old</p>
-            </div>
-            <div className="d-flex box2">
-                <div className="line1">
-                    <p>B</p>
-                </div>
-                <p className="para">45 BC, Make it Over </p>
-            </div>
-            <div className="d-flex box2">
-                <div className="line1">
-                    <p>C</p>
-                </div>
-                <p className="para">Lorem gimpk kollp ral</p>
-            </div>
-            <div className="d-flex box2">
-                <div className="line1">
-                    <p>D</p>
-                </div>
-                <p className="para">Kap hara rama hweo</p>
-            </div>
+            <Box></Box>
         </div>
     )
 }
